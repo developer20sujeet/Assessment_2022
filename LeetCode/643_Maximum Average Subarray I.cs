b86ba9490ae6fa0c8ffad152eb44bab76c21c0ad
sujeet kumar <developer20sujeet@gmail.com>
2022-05-03 11:02:27 -0500

    link

---
+++ LeetCode/643_Maximum Average Subarray I.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    
+    public  class Maximum_Average_Subarray_I
+    {
+
+        public float FindMaxAverage(int [] num , int k )
+        {
+            float max = int.MaxValue;
+
+
+
+
+            return 
+
+        }
+
+    }
+}
